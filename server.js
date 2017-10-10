902e8fe007eb35fb6f1930775e1fe7c40630d123
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-10-10 07:48:11 +0530

    Configuring expressjs

---
+++ server.js
@@ -0,0 +1,34 @@
+//Importing express, body-parser, path & http in constants
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+//constant app which binds to Express server
+const app = express();
+
+//API file for interacting with MongoDB
+const api = require('./server/routes/api');
+
+//Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//API Location
+app.use('/api', api);
+
+//Send all other requests to angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set port
+const port = process.env.PROD || '3000';
+app.use('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
